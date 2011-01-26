be15bc7e5d00dc04911660af85184010c0548cc6
JongMan Koo <jongman@gmail.com>
2011-01-26 13:22:48 -0600

    refactored importer a bit

--- algospot/judge/management/commands/import_algospot.py
+++ algospot/judge/management/commands/import_algospot.py
@@ -7,15 +7,20 @@ class Command(BaseCommand):
     args = '<JSON file path>'
     help = 'Import all data from a JSON file from mysqldumper.'
 
-    def handle(self, *args, **kwargs):
-        if len(args) != 1:
-            raise CommandError("Specify a single JSON file path.")
-        import myjson
-        all = myjson.loads(open(args[0]).read())
-        print "%d problems." % len(all["GDN_Problem"])
+    def import_problems(self, all):
+        Problem.objects.all().delete()
         for problem in all["GDN_Problem"]:
             del problem["no"] # for now
             del problem["author"] # for now
             p = Problem(**problem)
             p.save()
             print "Imported %s." % problem["name"]
+        print "Imported %d problems." % len(all["GDN_Problem"])
+
+    def handle(self, *args, **kwargs):
+        if len(args) != 1:
+            raise CommandError("Specify a single JSON file path.")
+        import myjson
+        all = myjson.loads(open(args[0]).read())
+
+        self.import_problems(all)
