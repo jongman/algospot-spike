0b54110fcf1a094c49a29cc362fc0b39b5d7b6ab
JongMan Koo <jongman@gmail.com>
2011-01-26 13:20:05 -0600

    problem importing works for now

---
+++ algospot/judge/management/commands/import_algospot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+from algospot.judge.models import *
+
+class Command(BaseCommand):
+    args = '<JSON file path>'
+    help = 'Import all data from a JSON file from mysqldumper.'
+
+    def handle(self, *args, **kwargs):
+        if len(args) != 1:
+            raise CommandError("Specify a single JSON file path.")
+        import myjson
+        all = myjson.loads(open(args[0]).read())
+        print "%d problems." % len(all["GDN_Problem"])
+        for problem in all["GDN_Problem"]:
+            del problem["no"] # for now
+            del problem["author"] # for now
+            p = Problem(**problem)
+            p.save()
+            print "Imported %s." % problem["name"]
--- migrators/mysqldumper.py
+++ migrators/mysqldumper.py
@@ -3,10 +3,14 @@
 
 import sys
 
-TABLES = {"problems": ["no", "uploaded", "opened", "author", "source",
-    "language", "name", "description", "input", "output", "sample_input",
-    "sample_output", "note", "judge_module", "time_limit", "memory_limit",
-    "accepted", "submissions"]}
+TABLES = {"GDN_Problem": ["no", ("ID", "key"), "state", "author",
+    "updated", "state", "author", "source", "name", "description",
+    "name", "description", "input", "output",
+    ("sampleinput", "sample_input"), ("sampleoutput", "sample_output"),
+    "note",
+    ("judgemodule", "judge_module"),
+    ("timelimit", "time_limit"),
+    ("memorylimit", "memory_limit"), "accepted", "submissions"]}
 
 def main():
     if len(sys.argv) != 5:
@@ -18,9 +22,13 @@ def main():
     conn = MySQLdb.connect(host, username, password, db)
     res = {}
     for table, columns in TABLES.iteritems():
-        conn.query("SELECT %s FROM %s;" % (",".join(columns), table))
+        old_cols = [col if isinstance(col, basestring) else col[0] for col in
+                columns]
+        new_cols = [col if isinstance(col, basestring) else col[1] for col in
+                columns]
+        conn.query("SELECT %s FROM %s;" % (",".join(old_cols), table))
         results = conn.store_result()
-        res[table] = [dict(zip(columns, results.fetch_row()[0]))
+        res[table] = [dict(zip(new_cols, results.fetch_row()[0]))
                 for i in xrange(results.num_rows())]
     import myjson
     print myjson.dumps(res)
