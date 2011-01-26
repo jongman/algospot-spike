ef2239b1965d75dd5a7cfd492faf5cb5dcfae591
JongMan Koo <jongman@gmail.com>
2011-01-26 16:23:19 -0600

    finally problem list works (without much style, though)

--- algospot/judge/urls.py
+++ algospot/judge/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.defaults import *
 import judge.views
 
 urlpatterns = patterns('',
-    url(r'$', 'judge.views.index', name='judge_index')
+    #url(r'$', 'judge.views.index', name='judge_index'),
+    url(r'listproblems/$', 'judge.views.listproblems',
+        name='judge_listproblems'),
 )
 
--- algospot/judge/views.py
+++ algospot/judge/views.py
@@ -1,5 +1,22 @@
 from annoying.decorators import render_to
+from models import Problem, Submission, PROBLEM_STATES, SUBMISSION_STATES
+from django.core.paginator import Paginator
+
+PROBLEMS_PER_PAGE = 25
 
 @render_to("index.html")
 def index(request):
     return {}
+
+@render_to("problem_list.html")
+def listproblems(request):
+    all = Problem.objects.filter(state=PROBLEM_STATES["PUBLISHED"])
+    all = all.order_by("key")
+
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        page = 1
+
+    paginator = Paginator(all, PROBLEMS_PER_PAGE)
+    return {"page": paginator.page(page)}
